bfb97f22d51a79a333fe9dc982364206dd82d21a
KRIS-25 <krisrego.sdcce@vvm.edu.in>
2023-01-06 12:15:10 +0530

    Added Assignment - 01

--- 01-JavaScript-Fundamentals-Part-1/script.js
+++ 01-JavaScript-Fundamentals-Part-1/script.js
@@ -1,3 +1,17 @@
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-console.log(100 + 100);
+/*
+    let js = "amazing";
+    if (js === "amazing") alert("JavaScript is FUN!");
+    console.log(100 + 100);
+*/
+
+console.log("Kris");
+console.log("23");
+
+let firstName = "Rakesh";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 1;
+console.log(PI);
---
+++ Assignments/01-Values-and-Variables/assignment.js
@@ -0,0 +1,7 @@
+let country = "India";
+let continent = "Asia";
+let population = "300 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
