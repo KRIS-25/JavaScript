ae2029cea99e1d941f56962685dcd5f5249cc356
KRIS-25 <krisrego.sdcce@vvm.edu.in>
2023-01-07 15:29:03 +0530

    added code challenge 3

--- 01-JavaScript-Fundamentals-Part-1/script.js
+++ 01-JavaScript-Fundamentals-Part-1/script.js
@@ -180,3 +180,120 @@ if (birthYear <= 2000) {
 
 console.log(century);
 */
+
+/*
+
+//Functions Number or String must start with a Capital N or S
+
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("kris"));
+console.log(typeof NaN); // Weird result -> Output -> number
+
+console.log(String(23), 23);
+
+// type coercion
+
+console.log("I am " + 20 + " years old!"); // converts the number to a string '+'
+console.log("23" - "10" - 3); // Output -> 13
+
+
+        // --> mius operator actually triggers the opposite conversion.
+        // So in this case strings are converted to numbers
+
+
+
+console.log("23" + "10" + 3); // Output -> 23103
+console.log("25" * "2");
+console.log("10" / "2");
+
+let n = "1" + 1;
+n = n - 1;
+console.log(n); // Output -> 11
+
+console.log(2 + 3 + 4 + "5"); // Output -> 95 -> which is a string
+console.log("10" - "4" - "3" - 2 + "5"); // Output -> 15 which is again a string
+
+
+*/
+
+/*
+
+// 5 falsy values: 0, "", undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Kris"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 0;
+if (money) {
+  console.log("Don't spend it all");
+} else {
+  console.log("You should get a job!");
+}
+
+let height;
+if (height) {
+  console.log("YAY!, Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
+*/
+
+/*
+const age = 18;
+
+if (age === 18) console.log("You just became an adult");
+
+console.log(18 == "18"); // loose equality operator -> does type coercion.  --> Output -> true
+console.log(18 === "18"); // strict equality operator - doesnot do type coercion --> Output -> false
+
+const favourite = Number(prompt("What's you favourite number")); // By default converts to string if not converted to number
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else if (favourite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7 or 9");
+}
+
+if (favourite !== 23) console.log("Why not 23?"); // !== -> Strict and != loose
+*/
+
+/*
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision); // Output -> true
+console.log(hasDriversLicense || hasGoodVision); // Output -> true
+console.log(!hasDriversLicense); // Output -> false
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+if (shouldDrive) {
+  console.log("Sarah is able to drive!"); // <- Output
+} else {
+  console.log("Someone else should drive....");
+}
+
+const isTried = false; // C
+
+console.log(hasDriversLicense && hasGoodVision && isTried); // Output -> false
+
+if (hasDriversLicense && hasGoodVision && !isTried) {
+  console.log("Sarah is able to drive!"); //  <- Output
+} else {
+  console.log("Someone else should drive...");
+}
+
+*/
